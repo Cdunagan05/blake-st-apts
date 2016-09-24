9c3b5f5ce06243418fafea6d522e230a0e5352d6
Chase Dunagan <cdunagan@utexas.edu>
2016-09-24 15:32:10 -0600

    Add feature test for owner show page

---
+++ spec/features/owners_show_their_units_info_on_their_page_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.feature "When I visit /owners" do
+  scenario "I see a link to click on a particular owners page" do
+    owner1 = Owner.create(first_name: "Jessica", last_name: "Jones")
+    owner2 = Owner.create(first_name: "Luke", last_name: "Cage")
+    owner1.units.create(name: "1a", monthly_rent: 900)
+    owner1.units.create(name: "1b", monthly_rent: 1100)
+    owner2.units.create(name: "2a", monthly_rent: 1500)
+
+    visit owners_path
+
+    click_link 'Jessica Jones'
+
+    expect(current_path).to eq('/owners/1')
+
+    expect(page).to have_content("Jessica Jones")
+    expect(page).to have_content("1a: 900 per month")
+    expect(page).to have_content("1b: 1100 per month")
+    expect(page).to have_content("Total rent paid to Jessica per month: $2000")
+
+  end
+end
