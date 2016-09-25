78973bd197e5851ddb21fa437052c2c4a6132e02
Chase Dunagan <cdunagan@utexas.edu>
2016-09-25 13:28:18 -0600

    add feature test for all renters and their rent info

---
+++ spec/features/visitor_sees_all_renters_and_rent_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.feature "When someone visits '/renters/'" do
+  scenario "they see all renters, their monthly rent, cost per sqft, and more" do
+    renter1 = Renter.create(first_name: "Jake", last_name: "Jones")
+    renter2 = Renter.create(first_name: "Adam", last_name: "Arroyo")
+    Unit.create(name: "1a", monthly_rent: 1500, square_footage: 1200, renter: renter1 )
+    Unit.create(name: "1b", monthly_rent: 1800, square_footage: 1300, renter: renter2 )
+
+    visit renters_path
+
+    expect(page).to have_content("Jake Jones: 1a - $1500 per month, $1.25 per sqft")
+    expect(page).to have_content("Adam Arroyo: 1b - $1800 per month, $1.38 per sqft")
+    expect(page).to have_content("$1.32 per sqft for all renters"
+
+  end
+end
